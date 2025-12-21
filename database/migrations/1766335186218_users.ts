bdb054c723d4eb5bc3b805876919ce1e5c9ea193
gilanghuda <gilanghuda99@gmail.com>
2025-12-22 00:13:29 +0700

    refactor: use uuid for user id

--- database/migrations/1766335186218_users.ts
+++ database/migrations/1766335186218_users.ts
@@ -4,8 +4,9 @@ export default class extends BaseSchema {
   protected tableName = 'users'
 
   public async up() {
+    this.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
       table.string('email', 255).notNullable().unique()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
